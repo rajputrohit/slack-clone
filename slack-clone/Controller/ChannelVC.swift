73f70d1c6876be80c5ee4669d276cf5b01a0d189
Rohit Rajput <rohitrajput@Rohits-MacBook-Pro.local>
2018-01-19 11:22:12 +0530

    Scanner build to scan color string, ex. [ 0.5, 0.5, 0.5, 1.0]

--- slack-clone/Controller/ChannelVC.swift
+++ slack-clone/Controller/ChannelVC.swift
@@ -11,11 +11,13 @@ import UIKit
 class ChannelVC: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var userImage: CircleImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(userDataDidChange(_:)), name: NOTIFY_USER_DATA_DID_CHANGE, object: nil)
     }
     
     @IBAction func loginButtonDidPress(_ sender: Any) {
@@ -24,6 +26,19 @@ class ChannelVC: UIViewController {
     
     @IBAction func unwindFromCreateAccountVC(segue: UIStoryboardSegue){
         
+    }
+    
+    @objc func userDataDidChange(_ notify: Notification) {
+        if (AuthService.instance.isLoggedIn) == true {
+            loginButton.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named:(UserDataService.instance.avatarName))
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+            
+        } else {
+            loginButton.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
     }
     
 }
